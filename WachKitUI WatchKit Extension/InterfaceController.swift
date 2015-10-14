9f22258b4055c3c9d2238ed1bebb0905bfff3373
–Т–Є–Ї—В–Њ—А –Ь—П—Б–Њ–µ–і–Њ–≤ <victor.myasoedov@gmail.com>
2015-10-14 15:54:59 +0500

    Initial Commit

---
+++ WachKitUI WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WachKitUI WatchKit Extension
+//
+//  Created by s1n7e3 on 14.10.15.
+//  Copyright © 2015 s1n7e3.net. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
